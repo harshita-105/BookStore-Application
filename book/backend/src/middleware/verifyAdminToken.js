31b0bfd941d2e2a239c11e4f71437bbfb4429f1d
harshita-105 <186862827+harshita-105@users.noreply.github.com>
2026-05-18 22:18:13 +0530

    -

--- book/backend/src/middleware/verifyAdminToken.js
+++ book/backend/src/middleware/verifyAdminToken.js
@@ -5,12 +5,12 @@ const verifyAdminToken= (req,res, next)=>{
   const token= req.headers['authorization']?.split(' ')[1];
 
   if(!token){
-    return res.status(401).json({messsage: "Access denied."});
+    return res.status(401).json({message: "Access denied."});
   }
 
   jwt.verify(token,JWT_SECRET, (err,user)=>{
     if(err){
-      return res.status(403).json({messsage: "Invalid credentials."})
+      return res.status(403).json({message: "Invalid credentials."})
     }
     req.user=user;
     next();
