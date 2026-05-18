7667b3a746f575e69a507e45c3e454912a3fc8f7
harshita-105 <186862827+harshita-105@users.noreply.github.com>
2026-05-18 22:16:35 +0530

    fixed bugs

--- book/backend/src/books/book.controller.js
+++ book/backend/src/books/book.controller.js
@@ -2,7 +2,7 @@ const Book=require('./book.model');
 
 const postBook =async(req,res)=>{
   try {
-   const newBook=await Book({...req.body});
+   const newBook=await new Book({...req.body});
    await newBook.save();   
    res.status(200).send({message:"Book posted.", book:newBook}) 
 
@@ -28,7 +28,7 @@ const getSingleBook= async(req,res)=>{
      const {id}=req.params;
      const book=await Book.findById(id);
      if(!book){
-      res.status(404).send({message:"Book not found"})  
+      return res.status(404).send({message:"Book not found"})  
      }
      res.status(200).send(book)
 
@@ -43,7 +43,7 @@ const updateBook=async(req,res)=>{
     const {id}=req.params;
     const updatedBook= await Book.findByIdAndUpdate(id, req.body, {new:true});
     if(!updatedBook){
-      res.status(404).send({message:"Book not found"}) 
+      return res.status(404).send({message:"Book not found"}) 
     }
     res.status(200).send({
       message: "Book updated successfully",
@@ -59,7 +59,11 @@ const updateBook=async(req,res)=>{
 const deleteABook= async(req,res)=>{
   try{
     const {id}=req.params;
-    const deletedBook= await Book.findByIdAndDelete(id)
+    const deletedBook= await Book.findByIdAndDelete(id);
+    if(!deletedBook){
+      return res.status(404).send({message:"Book not found"});
+    }
+    res.status(200).send({message:"Book deleted successfully", book: deletedBook});
 
   } catch(error){
     console.error("Error deleting book", error);
