243f022c66ee5f3d807b8d85f3f3dca2f88948fc
harshita-105 <186862827+harshita-105@users.noreply.github.com>
2026-05-20 19:31:47 +0530

    fiexd main.jsx

--- book/frontend/src/App.jsx
+++ book/frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
     <AuthProvide>
       <Navbar/>
-      <main className='min-h-screen max-w-screen-2xL max-auto px-4 py-6 font-primary'>
+      <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
         <Outlet/>
       </main>
       <Footer/>
